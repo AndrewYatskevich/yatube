4ba8452a8ac371b6fe730ab41a401fded12ee77f
Andrew Yatskevich <drew.yatskevich@gmail.com>
2022-06-12 22:51:37 +0300

    fixed pep warnings

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -9,17 +9,17 @@ from django.urls import reverse
 from ..models import Post, Group, User
 
 UPLOADED_IMG = SimpleUploadedFile(
-            name='small.gif',
-            content=(
-                b'\x47\x49\x46\x38\x39\x61\x02\x00'
-                b'\x01\x00\x80\x00\x00\x00\x00\x00'
-                b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-                b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-                b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-                b'\x0A\x00\x3B'
-            ),
-            content_type='image/gif'
-        )
+    name='small.gif',
+    content=(
+        b'\x47\x49\x46\x38\x39\x61\x02\x00'
+        b'\x01\x00\x80\x00\x00\x00\x00\x00'
+        b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+        b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+        b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+        b'\x0A\x00\x3B'
+    ),
+    content_type='image/gif'
+)
 POST_CREATE_URL = 'posts:post_create'
 POST_EDIT_URL = 'posts:post_edit'
 USER_USERNAME = 'TestName'
